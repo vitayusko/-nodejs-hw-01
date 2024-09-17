915ba3673ce6a3bf3daea66efdfdf76870991d86
vitayusko <vitayusko@users.noreply.github.com>
2024-09-17 14:26:14 +0200

    5-8 done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,6 +2,24 @@
 //У файлі src/scripts/countContacts.js опишіть функцію countContacts. Вона має повертати кількість контактів в масиві у файлі src/db/db.json.Переконайтесь, що функція точно рахує кількість контактів.
 //Перевірити роботу функції можна виконавши команду npm run count, яка буде виконувати код у файлі src/scripts/countContacts.js.
 
-export const countContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'fs';
+
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    if (!Array.isArray(contacts)) {
+      console.log('Файл не содержит массив контактов.');
+      return 0;
+    }
+    const contactCount = contacts.length;
+    console.log(`Количество контактов: ${contactCount}`);
+    return contactCount;
+  } catch (error) {
+    console.error('Ошибка при чтении файла:', error);
+    return 0;
+  }
+};
 
 console.log(await countContacts());
